6f93c76a95c2ce0c966c883f53ea8b0ec79168ad
kaydin <knguyen@financeit.io>
2020-09-16 21:08:06 -0400

    create some tables

---
+++ db/migrate/20200917005432_create_articles1.rb
@@ -0,0 +1,6 @@
+class CreateArticles1 < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles1s do |t|
+    end
+  end
+end
---
+++ db/migrate/20200917010625_add_back_title.rb
@@ -0,0 +1,6 @@
+class AddBackTitle < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :title, :string
+    add_column :articles, :description, :text
+  end
+end
